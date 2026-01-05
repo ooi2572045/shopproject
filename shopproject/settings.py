b35d35c2d54b4b96f92c1a1a38088c82c6cd5f36
ooi2572045 <ooi2572045@stu.o-hara.ac.jp>
2026-01-06 05:18:02 +0900

    fix allowed hosts

--- shopproject/settings.py
+++ shopproject/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'unsafe-default-key')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [shopproject-zsdo.onrender.com]
 
 
 
