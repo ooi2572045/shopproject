40646d2876ab390119daa73bbd92133fad9dbd4b
ooi2572045 <ooi2572045@stu.o-hara.ac.jp>
2026-01-06 17:46:04 +0900

    Fix startCommand for Render

--- shopproject/settings.py
+++ shopproject/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'unsafe-default-key')
 DEBUG = True
-ALLOWED_HOSTS = ['shopproject-zsdo.onrender.com', 'www.example.com']
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 
