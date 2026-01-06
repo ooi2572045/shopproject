39fd674434b4f5086e9603b092ff9350888a8c19
ooi2572045 <ooi2572045@stu.o-hara.ac.jp>
2026-01-06 17:11:39 +0900

    Enable DEBUG for error tracing

--- shopproject/settings.py
+++ shopproject/settings.py
@@ -4,7 +4,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'unsafe-default-key')
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = True
 ALLOWED_HOSTS = ['shopproject-zsdo.onrender.com', 'www.example.com']
 
 
