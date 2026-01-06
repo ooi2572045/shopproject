54c0e69fd2275bba1788e3da963cee255d670006
ooi2572045 <ooi2572045@stu.o-hara.ac.jp>
2026-01-06 16:35:00 +0900

    Fix whitenoise and settings

--- shopproject/settings.py
+++ shopproject/settings.py
@@ -54,11 +54,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'shopproject.wsgi.application'
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3'
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
